72f5f23d7f6d9ce938fa5add091002fffe59357e
Eirik Sæterøy <Eirik@EirikSterysMBP2.lan>
2020-09-09 11:45:20 +0200

    commit

--- Oblig1/oppgave1/Teque.java
+++ Oblig1/oppgave1/Teque.java
@@ -1,6 +1,46 @@
-class Teque {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-    public static void main(String[] args) {
-        
+class Teque {
+    static ArrayList<Integer> tall = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String[] line = br.readLine().split(" ");
+            String cmd = line[0];
+            int x = Integer.parseInt(line[1]);
+
+            if (cmd.length() == 10) {
+                push_front(x);
+            }
+            else if (cmd.length() == 9) {
+                push_back(x);
+            }
+            else if (cmd.length() == 11) {
+                push_middle(x);
+            }
+            else if (cmd.length() == 3) {
+                get(x);
+            }
+
+        }
+    }
+
+    public static void push_front(int i) {
+        tall.add(0, i);
+    }
+
+    public static void push_back(int i){
+        tall.add(i);
+    }
+    public static void push_middle(int i){
+        tall.add(((tall.size()+1)/2), i);
+    }
+    public static void get(int i){
+        System.out.println(tall.get(i));
     }
 }
