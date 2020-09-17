c4f04c37de99d45fbd9cfc4737793e0b27d33270
johasko <43589712+johasko@users.noreply.github.com>
2020-09-17 17:04:45 +0200

    Final fix

--- Oblig1/oppgave1/Teque.java
+++ Oblig1/oppgave1/Teque.java
@@ -4,11 +4,22 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 class Teque {
-    static ArrayList<Integer> tall = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        new TequeHandler().Handler(br);
+
+    }
+}
+
+class TequeHandler {
+    ArrayList<Integer> tall = new ArrayList<>();
+
+    public void Handler(BufferedReader br) throws IOException {
+
+
         int N = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < N; i++) {
             String[] line = br.readLine().split(" ");
             String cmd = line[0];
@@ -26,21 +37,20 @@ class Teque {
             else if (cmd.length() == 3) {
                 get(x);
             }
-
         }
     }
 
-    public static void push_front(int x) {
-        tall.add(0, x);
+    public void push_front(int i) {
+        tall.add(0, i);
     }
 
-    public static void push_back(int x){
-        tall.add(x);
+    public void push_back(int i){
+        tall.add(i);
     }
-    public static void push_middle(int x){
-        tall.add(((tall.size()+1)/2), x);
+    public void push_middle(int i){
+        tall.add(((tall.size()+1)/2), i);
     }
-    public static void get(int i){
+    public void get(int i){
         System.out.println(tall.get(i));
     }
 }
