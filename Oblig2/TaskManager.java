ae6a161eb656eac2ad43cb38fc152169fd1e224c
Eirik Sæterøy <Eirik@EirikSterysMBP2.lan>
2020-09-28 20:10:07 +0200

    Påbegynt TaskManager

---
+++ Oblig2/TaskManager.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class TaskManager {
+    int tasks;
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        tasks = Integer.parseInt(br.readLine());
+
+        for (String line = br.readLine(); line != null; line = br.readLine()) {
+            if () {
+
+            }
+        }
+    }
+}
