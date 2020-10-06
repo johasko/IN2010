79717a841e05cddaf1e072070ac9adba96881a36
Johannes Skøien <johannes_skoien@hotmail.com>
2020-10-06 20:41:22 +0200

    Implemented realizable()

--- Oblig2/TaskManager.java
+++ Oblig2/TaskManager.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.*;
 
 class TaskManager {
 
@@ -32,25 +33,45 @@ class TaskManager {
             }
         }
 
-        for (Task t : tasks) {
+        Task[] sykel = realizable(tasks);
+        if (sykel == null) {
+            System.out.println("Grafen er en sykel");
+        } else {
+            for (Task t : sykel) {
+                System.out.println(t.name);
+            }
+        }
+        /*for (Task t : tasks) {
             System.out.println(t);
-        }
+        }*/
     }
 
-    public boolean DFS(Task[] a, Task t) { 
-        t.status = true;
-        for (Task v : t.outEdges) {
-            if (v.status == false) {
-                DFS(a, v); 
-            }
-        }
-    }
 
-    public boolean DFS_full(Task[] a) {
-        for (Task v : a) {
-            if (v.status == false) {
-                DFS(a, v);
+
+    public static Task[] realizable(Task[] g) {
+        Stack<Task> s = new Stack<>();
+        Task[] output = new Task[g.length];
+
+        for (Task t : g) {
+            if (t.cntPredecessors == 0) {
+                s.push(t);
             }
         }
+        int i =1;
+        while (!s.empty()) {
+            Task v = s.pop();
+            output[i-1] = v;
+            i++;
+            for (Task e : v.outEdges) {
+                e.cntPredecessors--;
+                if (e.cntPredecessors == 0) {
+                    s.push(e);
+                }
+            }
+        }
+        if (i > g.length) {
+            return output;
+        }
+        return null;
     }
 }
