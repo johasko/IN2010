a58096afeae8e4c07a963c39b7b75225788e9dc3
Johannes Skøien <johannes_skoien@hotmail.com>
2020-10-29 13:23:59 +0100

    Start

--- Oblig3/Prekode/Quick.java
+++ Oblig3/Prekode/Quick.java
@@ -2,6 +2,9 @@ class Quick extends Sorter {
 
     void sort() {
 
+        int high = A.length-1;
+        int low = 0;
+
         // Do insertion sort here. Use the Sorter's comparison- and swap
         // methods for automatically counting the swaps and comparisons.
         //
@@ -11,7 +14,48 @@ class Quick extends Sorter {
         // Alternatively, you can "manually" increment the fields `comparisons'
         // and `swaps' at your own leisure.
 
+        quickSort(low, high);
+
+
     }
+
+    int[] quickSort(int low, int high) {
+        if (low >= high) {
+            return A;
+        }
+        int p = part(low, high);
+        quickSort(low, p-1);
+        quickSort(p+1, high);
+        return A;
+    }
+
+    int part(int low, int high) {
+        int p = (int)(Math.random() * (high - low + 1) + low);
+        swap(p, high);
+
+        int pivot = A[high];
+        int left = low;
+        int right = high-1;
+
+        while(left <= right) {
+            while(left <= right && leq(A[left], pivot)) {
+                left++;
+                System.out.println("Left = " + left);
+            }
+
+            while (right >= left && geq(A[right], pivot)) {
+                right++;
+                System.out.println("Right = " + right);
+            }
+
+            if (left < right) {
+                swap(left, right);
+            }
+        }
+        swap(left, high);
+        return left;
+    }
+
 
     String algorithmName() {
         return "quick";
