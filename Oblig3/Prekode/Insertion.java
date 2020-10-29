c427b5abaacb178d2a46923fbf0d4c0b83900c4c
Johannes Skøien <johannes_skoien@hotmail.com>
2020-10-29 22:08:24 +0100

    Fixed insertion-sort

--- Oblig3/Prekode/Insertion.java
+++ Oblig3/Prekode/Insertion.java
@@ -1,16 +1,7 @@
 class Insertion extends Sorter {
 
     void sort() {
-
-        // Do insertion sort here. Use the Sorter's comparison- and swap
-        // methods for automatically counting the swaps and comparisons.
-        //
-        // The object has a field A and n, where A is to be sorted and n is its
-        // length.
-        //
-        // Alternatively, you can "manually" increment the fields `comparisons'
-        // and `swaps' at your own leisure.
-        for (int i = 1; i < n-1; i++) {
+        for (int i = 1; i < n; i++) {
             int j = i;
             while (j > 0 && gt(A[j-1], A[j])) {
                 swap(j-1, j);
