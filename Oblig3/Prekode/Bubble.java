0e2f1940cd9f5edda0c7cbca44a523367d48cc45
Johannes Skøien <johannes_skoien@hotmail.com>
2020-11-02 14:03:37 +0100

    Updated bubble-sort with compare-methods

--- Oblig3/Prekode/Bubble.java
+++ Oblig3/Prekode/Bubble.java
@@ -5,7 +5,7 @@ class Bubble extends Sorter {
         for (int i = 0; i < n-1; i++) {
             int count = 0;
             for (int j = 0; j < n-i-1; j++) {
-                if (A[j] > A[j+1]) {
+                if (gt(A[j], A[j+1])) {
                     swap(j, j+1);
                     count++;
                 }
