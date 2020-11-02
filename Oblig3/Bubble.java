689b63e83272d05c66fc3513d43ae4d89b78674a
Johannes Skøien <johannes_skoien@hotmail.com>
2020-11-03 00:28:14 +0100

    Bubble sort-algorithm

---
+++ Oblig3/Bubble.java
@@ -0,0 +1,19 @@
+class Bubble extends Sorter {
+
+    void sort() {
+
+        for (int i = 0; i < n-1; i++) {
+            int count = 0;
+            for (int j = 0; j < n-i-1; j++) {
+                if (gt(A[j], A[j+1])) {
+                    swap(j, j+1);
+                    count++;
+                }
+            }
+        }
+    }
+
+    String algorithmName() {
+        return "bubble";
+    }
+}
